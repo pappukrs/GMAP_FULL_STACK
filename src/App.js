611c919732cd886d80cef1d562089bd397d32c36
pappukrs <digitalpappu101@gmail.com>
2024-07-27 18:07:39 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
+import React, { useState, useEffect, useRef } from 'react';
+import { MapContainer, TileLayer, Marker, Polyline, useMap } from 'react-leaflet';
 import io from 'socket.io-client';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+const vehicleIcon = (angle) => new L.Icon({
+    iconUrl: 'https://vectorified.com/image/car-png-vector-33.png', 
+    iconSize: [100, 100], 
+    iconAnchor: [50, 50], 
+    className: 'rotating-icon', 
+});
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
+});
 
 const containerStyle = {
     width: '100%',
     height: '100vh'
 };
 
-const center = {
-    lat: 17.385044,
-    lng: 78.486671
-};
+const center = [17.385044, 78.486671]; 
+
 
 const App = () => {
     const [vehicleData, setVehicleData] = useState([]);
     const [path, setPath] = useState([]);
+    const [rotation, setRotation] = useState(0); 
 
     useEffect(() => {
         const socket = io('http://localhost:3000');
@@ -24,36 +39,67 @@ const App = () => {
             console.log("data", data);
         });
 
+        socket.on('alka', (data) => {
+            console.log("alka", data);
+        });
+
         return () => socket.disconnect();
     }, []);
 
     useEffect(() => {
         if (vehicleData.length) {
-            const newPath = vehicleData.map(point => ({
-                lat: point.latitude,
-                lng: point.longitude
-            }));
+            const newPath = vehicleData.map(point => [point.latitude, point.longitude]);
             setPath(newPath);
             console.log("path", newPath);
+            
+            
+            if (newPath.length > 1) {
+                const angle = getAngle(newPath[newPath.length - 2], newPath[newPath.length - 1]);
+                setRotation(angle);
+            }
         }
     }, [vehicleData]);
 
+    const getAngle = (prev, current) => {
+        const latDiff = current[0] - prev[0];
+        const lngDiff = current[1] - prev[1];
+        return Math.atan2(latDiff, lngDiff) * (180 / Math.PI); 
+    };
+
+    const MapUpdater = () => {
+        const map = useMap();
+        const prevPathLength = useRef(path.length);
+
+        useEffect(() => {
+            if (path.length) {
+                
+                map.setView(path[path.length - 1], 15); 
+                prevPathLength.current = path.length;
+            }
+        }, [path, map]);
+
+        return null;
+    };
+
     return (
-        <LoadScript googleMapsApiKey="AIzaSyAoH5aK6Qu8D5ECB72BzC3UbEXlyAc41GY">
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={15}
-                key={path.length} // force re-render when path changes
-            >
+        <div style={containerStyle}>
+            <MapContainer center={center} zoom={15} style={containerStyle}>
+                <MapUpdater />
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
                 {path.length > 0 && (
                     <>
-                        <Marker position={path[path.length - 1]} />
-                        <Polyline path={path} options={{ strokeColor: '#FF0000' }} />
+                        <Marker 
+                            position={path[path.length - 1]} 
+                            icon={vehicleIcon(rotation)} 
+                            rotationAngle={rotation} 
+                        />
+                        <Polyline positions={path} color="red" />
                     </>
                 )}
-            </GoogleMap>
-        </LoadScript>
+            </MapContainer>
+        </div>
     );
 };
 
---
+++ src/Appt.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
+import io from 'socket.io-client';
+
+const containerStyle = {
+    width: '100%',
+    height: '100vh'
+};
+
+const center = {
+    lat: 17.385044,
+    lng: 78.486671
+};
+
+const App = () => {
+    const [vehicleData, setVehicleData] = useState([]);
+    const [path, setPath] = useState([]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3000');
+
+        socket.on('vehicleLocation', (data) => {
+            setVehicleData(prevData => [...prevData, data]);
+            console.log("data", data);
+        });
+        socket.on('alka', (data) => {
+            //setVehicleData(prevData => [...prevData, data]);
+            console.log("alka", data);
+        });
+
+        return () => socket.disconnect();
+    }, []);
+
+    
+    
+
+    useEffect(() => {
+        if (vehicleData.length) {
+            const newPath = vehicleData.map(point => ({
+                lat: point.latitude,
+                lng: point.longitude
+            }));
+            setPath(newPath);
+            console.log("path", newPath);
+        }
+    }, [vehicleData]);
+
+    return (
+        <LoadScript googleMapsApiKey="AIzaSyAoH5aK6Qu8D5ECB72BzC3UbEXlyAc41GY">
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={15}
+                key={path.length} // force re-render when path changes
+            >
+                {path.length > 0 && (
+                    <>
+                        <Marker position={path[path.length - 1]} />
+                        <Polyline path={path} options={{ strokeColor: '#FF0000' }} />
+                    </>
+                )}
+            </GoogleMap>
+        </LoadScript>
+    );
+};
+
+export default App;
